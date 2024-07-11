228b5f42f0adcaa259ddfcb44a4296a56f5d1be6
Jonathan farias de Freitas <jf5423043@gmail.com>
2024-07-10 21:26:33 -0300

    segunda versão

--- spectacular_snaget_bruticus1/spectacular_snaget_bruticus1.ino
+++ spectacular_snaget_bruticus1/spectacular_snaget_bruticus1.ino
@@ -5,11 +5,13 @@ liquidCrystal_I2C ldc(0x20,16,2);
 void setup(){
 ldc.init();
   ldc.backlight();
-  ldc.setCursor(0 , 0);
-  ldc.print('Hello,World!");
-            }
+  }
             
 void loop(){
  // o "void loop" sempre precisa star presente mesmo que não use ele.
-  
+  for(int i = 0 ; i <= 16; i++){
+    lcd.clear();
+    lcd.setCursor(i , 0);
+    lcd("Hello,World!!");
+  }
 }
